bf1a97431a9997331af50fad6782a34384c64d4c
Isabella Chiaravalloti <55116527+isabellachiarav@users.noreply.github.com>
2019-10-23 20:02:46 -0400

    version 2

--- GroceryDiscount.java
+++ GroceryDiscount.java
@@ -4,45 +4,51 @@ import java.util.Scanner;
 public class GroceryDiscount {
 
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
 		NumberFormat formatter = NumberFormat.getCurrencyInstance();
-		
-		int continueGame = 1;
-		
-		while (continueGame == 1){
-			
-			System.out.print("Enter the cost of your groceries: ");
+
+		System.out.print("Enter the cost of your groceries or 'E' to end: ");
+
+		while (in.hasNextDouble()) {
+
 			double groceryCost = in.nextDouble();
 			
-			if (groceryCost < 10) {
-				System.out.println("You receive no coupon.");
-			}
-			else if (groceryCost <= 60) {
-				double discount = 8;
-				System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
-						" (" + discount + "% of your purchase)");
-			}
-			else if (groceryCost <= 150) {
-				double discount = 10;
-				System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
-						" (" + discount + "% of your purchase)");
-			}
-			else if (groceryCost <= 210) {
-				double discount = 12;
-				System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
-						" (" + discount + "% of your purchase)");
-			}
-			else {
-				double discount = 14;
-				System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
-						" (" + discount + "% of your purchase)");
-			}
-			System.out.print("Would you like to calculate more coupons? Enter 1 for yes and 0 for no: ");
-			continueGame = in.nextInt();
-			
-		}
+			if (groceryCost >= 10 && groceryCost <= 1000 ) {
 
+				if (groceryCost < 10) {
+					System.out.println("You receive no coupon.");
+				}
+				else if (groceryCost <= 60) {
+					double discount = 8;
+					System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
+							" (" + discount + "% of your purchase)");
+				}
+				else if (groceryCost <= 150) {
+					double discount = 10;
+					System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
+							" (" + discount + "% of your purchase)");
+				}
+				else if (groceryCost <= 210) {
+					double discount = 12;
+					System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
+							" (" + discount + "% of your purchase)");
+				}
+				else {
+					double discount = 14;
+					System.out.println("You receive a coupon for " + formatter.format(groceryCost * (discount*0.01)) + 
+							" (" + discount + "% of your purchase)");
+				}
+				
+			}
+			
+			else {
+				System.out.println("Input out of range.");
+			}
+
+			System.out.print("Enter the cost of your groceries or 'E' to end: ");
+		}	
 	}
 
 }
+
