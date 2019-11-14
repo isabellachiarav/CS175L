04c87725bcbb736bcd5e58739d86117c31d3bcde
Isabella Chiaravalloti <55116527+isabellachiarav@users.noreply.github.com>
2019-11-13 19:57:08 -0500

    Add files via upload

--- MultiplicationTable1.java
+++ MultiplicationTable1.java
@@ -3,26 +3,31 @@ public class MultiplicationTable1 {
 
 	public static void main(String[] args) {
 
-		//prints a 10x10 multiplication table
-		int i, j, sum, max = 10, sumMax = 100;
-		for (i = 1; i <= max; i++) {
-			for (j = 1; j <= max; j++) {
-				sum = i * j;
-				System.out.print(sum);
-				if(sum < max){
-					System.out.print("    ");
+				//prints first two rows
+				int i, j, sum, max = 10, sumMax = 100;
+				System.out.print("   ");
+				for (i = 1; i <= max; i++) {
+					System.out.printf("%5d", i);
 				}
-				else if(sum >= sumMax){
-					System.out.print("  ");
+				System.out.println();
+				System.out.println("______________________________________________________");
+				
+				//main multiplication table
+				for (i = 1; i <= max; i++) {
+					if (i < 10) {
+						System.out.print(" " + i + "|");
+					}
+					else {
+						System.out.print(i + "|");
+					}
+					for (j = 1; j <= max; j++) {
+						sum = i * j;
+						System.out.printf("%5d", sum);
+					}
+					System.out.println();
 				}
-				else if(sum >= max){
-					System.out.print("   ");
-				}
-			}
-			System.out.println();
-		}
 
+		}
 
 	}
 
-}
